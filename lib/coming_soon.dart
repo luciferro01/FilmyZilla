fb2b35176b1560c0368551ad5bfdaead2c76b51d
luciferro01 <lucifer1111ronaldo@gmail.com>
2022-09-03 00:27:37 +0530

    Removing an Issue.

--- lib/coming_soon.dart
+++ lib/coming_soon.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const String apiKey = 'fb1bf72f316f1126ca9f4258076532c7';
+const String accessToken =
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYjFiZjcyZjMxNmYxMTI2Y2E5ZjQyNTgwNzY1MzJjNyIsInN1YiI6IjYzMGY0ZDIwY2I4MDI4MDA5NGZiMDRhZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dRKPDbdlM1dRovAqCO1RdgC5RMj6VAu5Shk8UOUw_YI';
+const String tmdbUrl = 'https://api.themoviedb.org/3/movie/550?api_key=$apiKey';
 
 class ComingSoon extends StatelessWidget {
-  const ComingSoon({Key? key}) : super(key: key);
+  Future getDetails() async {
+    var response = await http.get(Uri.parse(tmdbUrl));
+    String data = response.body;
+    var decodeData = jsonDecode(data);
+    print(response.body);
+    print(decodeData);
+    // return decodeData;
+    // Response response = await http.get(tmdb_api_url);
+    // const data = await response.json();
+    // const { name, first_air_date, vote_average, number_of_seasons, episode_run_time, genres, overview, origin_country} = data;
+
+    // document.getElementById('genres').textContent = genres[i].name;
+  }
 
   @override
   Widget build(BuildContext context) {
+    getDetails();
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -14,7 +34,17 @@ class ComingSoon extends StatelessWidget {
             style: GoogleFonts.poppins(),
           ),
         ),
+        body: ListView(
+          children: [Container()],
+        ),
       ),
     );
   }
 }
+
+
+
+// const tmdb_api_url = "https://api.themoviedb.org/3/tv/" + 127332 + "?api_key=API-KEY";
+
+
+// getDetails();
